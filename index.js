199a86f85a4e631bc5256b39d71e97edcdd695e9
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-09-19 18:00:43 -0700

    starting to build proxy

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from "cors";
+import dotenv from "dotenv";
+import emailjs from "emailjs-com"
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
