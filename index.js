edaf1b6ea58cd891ac3ba3ffcc4bd22abbc5a918
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-09-20 12:08:23 -0700

    got it working by using axios and the API emailjs.com provides

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from "cors";
 import dotenv from "dotenv";
-import emailjs from "emailjs-com"
+import axios from "axios";
 
 dotenv.config()
 
 const app = express()
 const port = 3000
 const USER_ID = process.env.USER_ID
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN
 
 
 app.use(cors())
@@ -15,17 +16,18 @@ app.use(express.json())
 
 const sendEmail = async (req, res) => {
     const emailContent = req.body
-    console.log(emailContent)
+    const data = {
+        service_id: 'default_service',
+        template_id: 'template_rltdtcs',
+        user_id: USER_ID,
+        accessToken: ACCESS_TOKEN,
+        template_params: emailContent
+    }
     try {
-        await emailjs.send(
-            'default_service',
-            'template_rltdtcs',
-            emailContent,
-            USER_ID
-        )
+        await axios.post('https://api.emailjs.com/api/v1.0/email/send', data)
         res.status(201).json({ message: 'Your message was successful', email: emailContent})
     } catch (error) {
-        res.status(400).json({ error: error.message })
+        res.status(400).json({ error: error })
     }
 
 }
